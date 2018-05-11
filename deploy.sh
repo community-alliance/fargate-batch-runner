3c7ab573c94b36a874c3008709068797ba70acb4
larse514 <andrew.larsen@concordusa.com>
2018-05-10 20:44:38 -0500

    add some stuff (sorry)

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ STACK_NAME=$2
 S3_BUCKET=$3
 CONTAINER_NAME=$4
 SERVICE_NAME=$5
-BUCKET_NAME=$6
-ECS_CLUSTER=$7
-IMAGE=$8
-TASK_ROLE_ARN=$9
+ECS_CLUSTER=$6
+IMAGE=$7
+TASK_ROLE_ARN=$8
+TASK_SUBNETS=$9
 echo "Packing assets"
 ##
 # Package API Gateway Assets
@@ -27,5 +27,5 @@ aws cloudformation deploy --template-file \
     lambda-s3-trigger-output.yml --capabilities CAPABILITY_IAM \
     --stack-name ${STACK_NAME}  --parameter-overrides \
     BucketName=${S3_BUCKET} ContainerName=${CONTAINER_NAME} \
-    ServiceName=${SERVICE_NAME} BucketName=${BUCKET_NAME} \
-    ECSCluster=${ECS_CLUSTER} Image=${Image} TaskRoleArn=${TASK_ROLE_ARN}
+    ServiceName=${SERVICE_NAME} ECSCluster=${ECS_CLUSTER} \
+    Image=${IMAGE} TaskRoleArn=${TASK_ROLE_ARN} TaskSubnets=${TASK_SUBNETS}
