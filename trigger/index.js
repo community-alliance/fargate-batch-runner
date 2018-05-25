26d8717f75bdfc69edbe3c88f1d5707f59b33a7d
larse514 <andrew.larsen@concordusa.com>
2018-05-24 23:47:59 -0500

    add appropriate logging method

--- trigger/index.js
+++ trigger/index.js
@@ -16,7 +16,7 @@ exports.handler = (event, context, callback) => {
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
     const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
-    logger.log("About to create definition with ", bucket, key, config)
+    logger.info("About to create definition with ", bucket, key, config)
     let params = task.createDefinition(bucket, key, config);
 
     logger.info("created params ", params)
