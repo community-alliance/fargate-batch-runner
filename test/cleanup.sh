fcf3d9cfd5b2dd028434129cce1a833d175ac974
larse514 <andrew.larsen@concordusa.com>
2018-05-26 12:12:13 -0500

    add set -e to bash script

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+
 ${ROLE_STACK}
 
 aws cloudformation delete-stack --stack-name ${ROLE_STACK}
--- test/test-s3-trigger.sh
+++ test/test-s3-trigger.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 TRIGGER_BUCKET=$1
 TRIGGER=$2
 NEW_BUCKET=$3
